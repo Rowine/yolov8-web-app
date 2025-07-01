674c924d05d0bfe7b265138a66b59cc17030d125
Rowine <rowinemabiog24@gmail.com>
2025-07-01 10:45:45 +0800

    fix issue: update detections

--- src/config/constants.js
+++ src/config/constants.js
@@ -106,7 +106,7 @@ export const APP_CONFIG = {
  * @type {Object}
  */
 export const MODEL_CONFIG = {
-  confidenceThreshold: 0.75, // 75% confidence threshold
+  confidenceThreshold: 0.50, // 75% confidence threshold
   nmsIoUThreshold: 0.45, // Non-maximum suppression IoU threshold
   maxDetections: 500, // Maximum number of detections to return
 }; 
